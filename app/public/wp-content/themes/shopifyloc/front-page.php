53177c9c0d110299ecb5c10a76b61c7f889532ed
LaupWing <loc_strict@hotmail.com>
2023-10-14 15:17:38 +0200

    ydoate

--- app/public/wp-content/themes/shopifyloc/front-page.php
+++ app/public/wp-content/themes/shopifyloc/front-page.php
@@ -9,8 +9,11 @@
          alt="shopify work station"
       >
    </div>
-   <div class="flex-1 flex-shrink-0 border-l border-black">
-
+   <div class="flex-1 flex-shrink-0 border-l border-black flex items-center justify-center">
+      <div class="max-w-sm w-full">
+         <h2 class="italic text-2xl font-semibold text-slate-900">Lorem ipsum dolor</h2>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id in cupiditate porro velit ullam nisi ipsum aliquid autem omnis officiis ipsa ducimus odit numquam accusamus corporis, veniam sed magni. Explicabo.</p>
+      </div>
    </div>
 </div>
 
