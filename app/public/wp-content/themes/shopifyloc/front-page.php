5a72f18d43380c4ede39cb4f55b622a53cff3c58
LaupWing <loc_strict@hotmail.com>
2023-10-14 15:21:23 +0200

    udpated

--- app/public/wp-content/themes/shopifyloc/front-page.php
+++ app/public/wp-content/themes/shopifyloc/front-page.php
@@ -11,10 +11,19 @@
    </div>
    <div class="flex-1 flex-shrink-0 border-l border-black flex items-center justify-center">
       <div class="max-w-sm w-full">
-         <h2 class="italic text-2xl font-semibold text-slate-900">Lorem ipsum dolor</h2>
+         <h2 class="italic text-3xl font-semibold text-slate-900">Lorem ipsum dolor</h2>
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id in cupiditate porro velit ullam nisi ipsum aliquid autem omnis officiis ipsa ducimus odit numquam accusamus corporis, veniam sed magni. Explicabo.</p>
       </div>
    </div>
+</div>
+
+<div class="mt-16 mx-auto flex flex-col w-full max-w-4xl">
+   <h2 class="text-3xl">Recent Blogs</h2>
+   <ul class="mt-6 grid grid-cols-3 gap-4">
+      <li class="p-2 bg-red-100"></li>
+      <li class="p-2 bg-red-100"></li>
+      <li class="p-2 bg-red-100"></li>
+   </ul>
 </div>
 
 <?php 
