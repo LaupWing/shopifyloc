6e64b8aa6678cd5f99b10bb7127b4eb87b6c12c9
LaupWing <loc_strict@hotmail.com>
2023-10-14 14:18:43 +0200

    udpated

--- app/public/wp-content/themes/shopifyloc/header.php
+++ app/public/wp-content/themes/shopifyloc/header.php
@@ -5,14 +5,15 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <?php wp_head() ?>
 </head>
-<body <?php body_class() ?>>
+<body class="bg-slate-50" <?php body_class() ?>>
    <header class="w-full flex bg-white border-b border-black">
       <h1 class="p-4">ShopifyLoc</h1>
       <nav class="border-x border-black flex">
-         <ul class="flex items-center">
-            <li class="px-4">Test</li>
-            <li class="px-4">Test</li>
-            <li class="px-4">Test</li>
+         <ul class="flex">
+            <li class="font-semibold bg-slate-900 text-slate-50 tracking-wider w-24 text-xs uppercase text-center flex items-center justify-center border-black border-r">Home</li>
+            <li class="font-semibold tracking-wider w-24 text-xs uppercase text-center flex items-center justify-center border-black border-r">Blog</li>
+            <li class="font-semibold tracking-wider w-24 text-xs uppercase text-center flex items-center justify-center border-black border-r">About</li>
+            <li class="font-semibold tracking-wider w-24 text-xs uppercase text-center flex items-center justify-center">Contact</li>
          </ul>
       </nav>
    </header>
