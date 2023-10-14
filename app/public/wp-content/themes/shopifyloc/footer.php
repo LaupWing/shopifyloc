f850dd1fb84d3802d6cc9f8f7ef24fffade182c2
LaupWing <loc_strict@hotmail.com>
2023-10-14 21:29:08 +0200

    udpate

--- app/public/wp-content/themes/shopifyloc/footer.php
+++ app/public/wp-content/themes/shopifyloc/footer.php
@@ -1,5 +1,18 @@
    <footer class="py-10 mt-10 bg-emerald-500">
-
+      <div class="mx-auto flex flex-col w-full max-w-5xl">
+         <button class="text-yellow-400 hover:scale-110 duration-200">
+            <?php get_template_part("templates/icons/social-instagram")?>
+         </button>
+         <button  class="text-yellow-400 hover:scale-110 duration-200">
+            <?php get_template_part("templates/icons/social-twitter")?>
+         </button>
+         <button  class="text-yellow-400 hover:scale-110 duration-200">
+            <?php get_template_part("templates/icons/social-youtube")?>
+         </button>
+         <button  class="text-yellow-400 hover:scale-110 duration-200">
+            <?php get_template_part("templates/icons/social-facebook")?>
+         </button>
+      </div>
    </footer>
 
    <?php wp_footer(); ?>
