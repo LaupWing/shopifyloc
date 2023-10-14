b2957d1b0b5e891a392bb282b7b60da5bd76d35b
LaupWing <loc_strict@hotmail.com>
2023-10-14 14:51:19 +0200

    udpate

--- app/public/wp-content/themes/shopifyloc/header.php
+++ app/public/wp-content/themes/shopifyloc/header.php
@@ -7,7 +7,17 @@
 </head>
 <body class="bg-slate-50" <?php body_class() ?>>
    <header class="w-full flex bg-white border-b border-black">
-      <h1 class="p-4">ShopifyLoc</h1>
+      <h1 class="p-4 flex text-slate-800 text-lg items-center">
+         <?php 
+            $custom_class = "w-5 h-5 text-emerald-500 mr-1";
+            get_template_part("templates/icons/logo" , null, array(
+               "custom_class" => $custom_class
+            ))
+         ?>
+         <i>
+            <strong>Shopify</strong>Loc
+         </i>
+      </h1>
       <nav class="border-x border-black flex">
          <ul class="flex">
             <li class="font-semibold bg-slate-900 text-slate-50 tracking-wider w-24 text-xs uppercase text-center flex items-center justify-center border-black border-r">Home</li>
