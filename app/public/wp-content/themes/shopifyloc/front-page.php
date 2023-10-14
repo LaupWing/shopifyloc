d0cbb738c7d6bba28676937fba6b7d7bd07afd4d
LaupWing <loc_strict@hotmail.com>
2023-10-14 15:07:01 +0200

    udpate

--- app/public/wp-content/themes/shopifyloc/front-page.php
+++ app/public/wp-content/themes/shopifyloc/front-page.php
@@ -1,7 +1,18 @@
 <?php 
    get_header()
 ?>
-<h2>Front page</h2>
+<div class="h-[80vh] flex">
+   <div class="flex flex-1">
+      <img 
+         class="object-cover" 
+         src="<?php echo get_the_post_thumbnail_url() ?>" 
+         alt="shopify work station"
+      >
+   </div>
+   <div class="flex-1 flex-shrink-0">
+
+   </div>
+</div>
 
 <?php 
    get_footer()
