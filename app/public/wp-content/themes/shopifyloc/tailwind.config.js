3678a120c9e14ae38bc1606520ed47fb0bf13651
LaupWing <loc_strict@hotmail.com>
2023-10-14 13:57:56 +0200

    udpated

--- app/public/wp-content/themes/shopifyloc/tailwind.config.js
+++ app/public/wp-content/themes/shopifyloc/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+   content: [
+      "./**/*.php",
+      "./src/**/*.jsx"
+   ],
+   theme: {
+      extend: {},
+   },
+   plugins: [
+      require("@tailwindcss/forms")
+   ],
 }
-
