1725be110bf7545361e3a14042ec5b15805dddf9
LaupWing <loc_strict@hotmail.com>
2023-10-15 17:44:21 +0200

    fix

--- app/public/wp-content/themes/shopifyloc/front-page.php
+++ app/public/wp-content/themes/shopifyloc/front-page.php
@@ -43,7 +43,10 @@
          
       ?>
             <li class="flex flex-col p-2 hover:border-black border border-transparent">
-               <a href="<?php the_permalink() ?>" class="flex flex-col">
+               <a 
+                  href="<?php the_permalink() ?>" 
+                  class="flex flex-col flex-1"
+               >
                   <div class="relative">
                      <div class="p-2 flex gap-2 absolute top-0 left-0">
                         <?php
