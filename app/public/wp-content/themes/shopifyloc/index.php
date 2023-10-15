f17b5a068be968aa2c0237cbd501312053276eec
LaupWing <loc_strict@hotmail.com>
2023-10-15 18:54:07 +0200

    update

--- app/public/wp-content/themes/shopifyloc/index.php
+++ app/public/wp-content/themes/shopifyloc/index.php
@@ -1,6 +1,22 @@
 <?php 
    get_header()
 ?>
+<div class="py-4 bg-slate-900">
+   <div class="mx-auto px-2 flex flex-col w-full max-w-5xl">
+      <h2 class="text-white text-2xl">Blogs</h2>
+   </div>
+</div>
+<div class="mx-auto mt-10 flex flex-col w-full max-w-5xl">
+   <ul class="grid grid-cols-3 gap-2">
+      <?php 
+         while(have_posts()){
+            the_post();
+            get_template_part("templates/cards/blog-card");
+      
+         }
+      ?>
+   </ul>
+</div>
 <div class="mt-10 mx-auto flex flex-col w-full items-center">
    Blogs
 </div>
