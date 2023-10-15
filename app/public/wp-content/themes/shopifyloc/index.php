ff8f7afc24ae0c50209a3dbf9a600b9414ef4212
LaupWing <loc_strict@hotmail.com>
2023-10-15 18:56:56 +0200

    udpate

--- app/public/wp-content/themes/shopifyloc/index.php
+++ app/public/wp-content/themes/shopifyloc/index.php
@@ -1,9 +1,10 @@
 <?php 
    get_header()
 ?>
-<div class="py-4 bg-slate-900">
+<div class="py-6 -mt-1 bg-slate-900">
    <div class="mx-auto px-2 flex flex-col w-full max-w-5xl">
-      <h2 class="text-white text-2xl">Blogs</h2>
+      <h2 class="text-slate-100 text-4xl">Blogs</h2>
+      <p class="text-slate-500 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
    </div>
 </div>
 <div class="mx-auto mt-10 flex flex-col w-full max-w-5xl">
@@ -17,10 +18,6 @@
       ?>
    </ul>
 </div>
-<div class="mt-10 mx-auto flex flex-col w-full items-center">
-   Blogs
-</div>
-
 <?php 
    get_footer()
 ?>
