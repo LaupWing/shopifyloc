c847cc7d1be8740897e558b857fd5be98f1c47a8
LaupWing <loc_strict@hotmail.com>
2023-10-15 17:59:54 +0200

    first pat of the blog

--- app/public/wp-content/themes/shopifyloc/single.php
+++ app/public/wp-content/themes/shopifyloc/single.php
@@ -3,8 +3,13 @@
 ?>
 <div class="mt-16 mx-auto flex flex-col w-full max-w-5xl">
    <div class="mx-auto text-center max-w-3xl flex flex-col">
-      <span class="mb-2 font-bold text-slate-500"><?php echo get_the_date() ?></span>
+      <span class="mb-2 font-semibold text-slate-400"><?php echo get_the_date() ?></span>
       <h2 class="font-bold text-3xl"><?php the_title() ?></h2>
+      <img 
+         class="aspect-video object-cover mt-4"
+         src="<?php echo get_the_post_thumbnail_url() ?>" 
+         alt="<?php echo get_the_post_thumbnail_caption()?>"
+      >
    </div>
 </div>
 
