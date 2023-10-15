1016a0a85f57103d4d9cf453cb5f5aef84fcbdb8
LaupWing <loc_strict@hotmail.com>
2023-10-15 18:15:01 +0200

    added more items

--- app/public/wp-content/themes/shopifyloc/single.php
+++ app/public/wp-content/themes/shopifyloc/single.php
@@ -6,9 +6,20 @@
       <span class="mb-2 font-semibold text-slate-400"><?php echo get_the_date() ?></span>
       <h2 class="font-bold text-3xl"><?php the_title() ?></h2>
       <p class="my-6"><?php echo get_the_excerpt() ?></p>
-      <div class="relative">
-         <div class="flex mx-auto absolute top-0 p-4">
-
+      <div class="relative flex">
+         <div class="flex w-full mx-auto absolute gap-4 top-0 p-4 flex-1 justify-center items-center">
+            <?php
+               $posttags = get_the_tags();
+               if ($posttags) {
+                  foreach($posttags as $tag) {
+            ?>
+               <div class="px-2 py-0.5 bg-white/40 backdrop-blur rounded text-emerald-800 text-xs uppercase font-bold">
+                  <?php echo $tag->name ?>
+               </div>
+            <?php 
+                  }
+               }
+            ?>
          </div>
          <img 
             class="aspect-video object-cover"
