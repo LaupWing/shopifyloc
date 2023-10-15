6c3fa6ff2442e9c13d3094c40b021d09a55281eb
LaupWing <loc_strict@hotmail.com>
2023-10-15 17:56:16 +0200

    udpate

--- app/public/wp-content/themes/shopifyloc/single.php
+++ app/public/wp-content/themes/shopifyloc/single.php
@@ -2,7 +2,10 @@
    get_header()
 ?>
 <div class="mt-16 mx-auto flex flex-col w-full max-w-5xl">
-   
+   <div class="mx-auto text-center max-w-3xl flex flex-col">
+      <span class="mb-2 font-bold text-slate-500"><?php echo get_the_date() ?></span>
+      <h2 class="font-bold text-3xl"><?php the_title() ?></h2>
+   </div>
 </div>
 
 <?php 
