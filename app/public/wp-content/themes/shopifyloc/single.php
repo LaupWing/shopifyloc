dd5256fb09cfe4ca0910a1e9927ee39c6b523193
LaupWing <loc_strict@hotmail.com>
2023-10-15 18:12:24 +0200

    abse contienare

--- app/public/wp-content/themes/shopifyloc/single.php
+++ app/public/wp-content/themes/shopifyloc/single.php
@@ -1,15 +1,21 @@
 <?php 
    get_header()
 ?>
-<div class="mt-16 mx-auto flex flex-col w-full max-w-5xl">
+<div class="mt-10 mx-auto flex flex-col w-full max-w-5xl">
    <div class="mx-auto text-center max-w-3xl flex flex-col">
       <span class="mb-2 font-semibold text-slate-400"><?php echo get_the_date() ?></span>
       <h2 class="font-bold text-3xl"><?php the_title() ?></h2>
-      <img 
-         class="aspect-video object-cover mt-4"
-         src="<?php echo get_the_post_thumbnail_url() ?>" 
-         alt="<?php echo get_the_post_thumbnail_caption()?>"
-      >
+      <p class="my-6"><?php echo get_the_excerpt() ?></p>
+      <div class="relative">
+         <div class="flex mx-auto absolute top-0 p-4">
+
+         </div>
+         <img 
+            class="aspect-video object-cover"
+            src="<?php echo get_the_post_thumbnail_url() ?>" 
+            alt="<?php echo get_the_post_thumbnail_caption()?>"
+         >
+      </div>
    </div>
 </div>
 
