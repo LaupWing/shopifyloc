8969483278fa0126ad2cb2fa47bf02bc7181d1ec
LaupWing <loc_strict@hotmail.com>
2023-10-16 19:24:46 +0200

    udpate

---
+++ app/public/wp-content/themes/shopifyloc/page-projects.php
@@ -0,0 +1,23 @@
+<?php 
+   get_header()
+?>
+<div class="py-6 -mt-1 bg-slate-900 z-50">
+   <div class="mx-auto px-2 flex flex-col w-full max-w-5xl">
+      <h2 class="text-slate-100 text-4xl">Projects</h2>
+      <p class="text-slate-500 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+   </div>
+</div>
+<div class="mx-auto mt-10 flex flex-col w-full max-w-5xl">
+   <ul class="grid grid-cols-3 gap-2">
+      <?php 
+         while(have_posts()){
+            the_post();
+            get_template_part("templates/cards/blog-card");
+      
+         }
+      ?>
+   </ul>
+</div>
+<?php 
+   get_footer()
+?>
