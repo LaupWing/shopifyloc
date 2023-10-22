7a8d35e27e63450b6f5a5a3c0590cfec3d6ec905
LaupWing <loc_strict@hotmail.com>
2023-10-22 09:15:05 +0200

    udpate

--- app/public/wp-content/themes/shopifyloc/index.php
+++ app/public/wp-content/themes/shopifyloc/index.php
@@ -10,10 +10,18 @@
 <div class="mx-auto my-10 flex flex-col w-full max-w-5xl">
    <ul class="grid grid-cols-3 gap-2">
       <?php 
-         while(have_posts()){
-            the_post();
-            get_template_part("templates/cards/blog-card");
-      
+         $args = array(
+            "category__not_in" => array( get_cat_ID("projects"))
+         );
+         
+         $custom_query = new WP_Query( $args );
+         
+         if ( $custom_query->have_posts() ) {
+            while ( $custom_query->have_posts() ) {
+               $custom_query->the_post();
+               get_template_part( "templates/cards/blog-card" );
+            }
+            wp_reset_postdata();
          }
       ?>
    </ul>
