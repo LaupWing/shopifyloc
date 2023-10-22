12c69c20e821fc01af655bd96b48bc2298433a99
LaupWing <loc_strict@hotmail.com>
2023-10-22 08:45:30 +0200

    upda5te

--- app/public/wp-content/themes/shopifyloc/page-about.php
+++ app/public/wp-content/themes/shopifyloc/page-about.php
@@ -1,9 +1,18 @@
 <?php 
    get_header()
 ?>
-<div class="mx-auto mt-10 flex flex-col w-full max-w-5xl">
-   <h2 class="uppercase text-2xl font-semibold tracking-wider">About <span class="text-emerald-500">me</span></h2>
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea mollitia totam sunt, numquam molestiae aliquam amet quos expedita ullam autem nihil error, sint eveniet suscipit natus, debitis labore omnis excepturi.</p>
+<div class="mx-auto mt-10 flex w-full max-w-5xl gap-10">
+   <div class="flex-1">
+      <h2 class="uppercase text-2xl font-semibold tracking-wider">About <span class="text-emerald-500">me</span></h2>
+      <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea mollitia totam sunt, numquam molestiae aliquam amet quos expedita ullam autem nihil error, sint eveniet suscipit natus, debitis labore omnis excepturi.</p>
+   </div>
+   <div class="flex-1">
+      <img 
+         src="https://th.bing.com/th/id/R.0dd78ca1e36ba07444782afa89a6e467?rik=J9NBKLDqBMj4tg&pid=ImgRaw&r=0" 
+         class="object-cover"
+         alt="profile photo"
+      >
+   </div>
 </div>
 <?php 
    get_footer()
