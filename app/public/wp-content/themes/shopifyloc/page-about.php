afc49ecc3ea757988995139ba052c05b517d87f5
LaupWing <loc_strict@hotmail.com>
2023-10-22 08:49:25 +0200

    update

--- app/public/wp-content/themes/shopifyloc/page-about.php
+++ app/public/wp-content/themes/shopifyloc/page-about.php
@@ -6,12 +6,15 @@
       <h2 class="uppercase text-2xl font-semibold tracking-wider">About <span class="text-emerald-500">me</span></h2>
       <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea mollitia totam sunt, numquam molestiae aliquam amet quos expedita ullam autem nihil error, sint eveniet suscipit natus, debitis labore omnis excepturi.</p>
    </div>
-   <div class="flex-1">
+   <div class="flex-1 relative">
       <img 
          src="https://th.bing.com/th/id/R.0dd78ca1e36ba07444782afa89a6e467?rik=J9NBKLDqBMj4tg&pid=ImgRaw&r=0" 
          class="object-cover"
          alt="profile photo"
       >
+      <div class="w-20 h-20 absolute bottom-0 left-0 bg-white rounded-full transform -translate-x-1/2 translate-y-1/2">
+
+      </div>
    </div>
 </div>
 <?php 
