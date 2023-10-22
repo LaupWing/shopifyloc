66c2920744621447bb24d7430911752e18f41ffb
LaupWing <loc_strict@hotmail.com>
2023-10-22 08:36:45 +0200

    aoubt me

--- app/public/wp-content/themes/shopifyloc/page-about.php
+++ app/public/wp-content/themes/shopifyloc/page-about.php
@@ -2,7 +2,7 @@
    get_header()
 ?>
 <div class="mx-auto mt-10 flex flex-col w-full max-w-5xl">
-   <h2>About</h2>
+   <h2 class="uppercase text-2xl font-semibold tracking-wider">About <span class="text-emerald-500">me</span></h2>
 </div>
 <?php 
    get_footer()
