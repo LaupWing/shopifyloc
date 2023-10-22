1342dc1e18be0aa97996408da0144306d71154c7
LaupWing <loc_strict@hotmail.com>
2023-10-22 18:52:25 +0200

    udpate

--- app/public/wp-content/themes/shopifyloc/header.php
+++ app/public/wp-content/themes/shopifyloc/header.php
@@ -5,7 +5,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <?php wp_head() ?>
 </head>
-<body class="bg-slate-50 min-h-screen flex flex-col" <?php body_class() ?>>
+<body class="bg-slate-50 min-h-screen w-screen flex flex-col" <?php body_class() ?>>
    <header class="w-full hidden sm:flex h-14 bg-white border-b border-black">
       <h1 class="px-4 flex text-slate-800 text-lg items-center">
          <?php 
@@ -25,6 +25,27 @@
             ]);
          ?>
       </nav>
+      <div class="ml-auto flex items-center border-l border-black px-6">
+         <?php 
+            $custom_class = "w-5 h-5 text-slate-800";
+            get_template_part("templates/icons/search" , null, array(
+               "custom_class" => $custom_class
+            ))
+         ?>
+      </div>
+   </header>
+   <header class="w-full sm:hidden flex h-14 bg-white border-b border-black">
+      <h1 class="px-4 flex text-slate-800 text-lg items-center">
+         <?php 
+            $custom_class = "w-5 h-5 text-emerald-500 mr-1";
+            get_template_part("templates/icons/logo" , null, array(
+               "custom_class" => $custom_class
+            ))
+         ?>
+         <i>
+            <strong>Shopify</strong>Loc
+         </i>
+      </h1>
       <div class="ml-auto flex items-center border-l border-black px-6">
          <?php 
             $custom_class = "w-5 h-5 text-slate-800";
