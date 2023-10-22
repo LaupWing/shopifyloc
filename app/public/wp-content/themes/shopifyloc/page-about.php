c42a409aae8ea5cca527c8f2281e5b33318b788c
LaupWing <loc_strict@hotmail.com>
2023-10-22 08:40:52 +0200

    udpate

--- app/public/wp-content/themes/shopifyloc/page-about.php
+++ app/public/wp-content/themes/shopifyloc/page-about.php
@@ -3,6 +3,7 @@
 ?>
 <div class="mx-auto mt-10 flex flex-col w-full max-w-5xl">
    <h2 class="uppercase text-2xl font-semibold tracking-wider">About <span class="text-emerald-500">me</span></h2>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea mollitia totam sunt, numquam molestiae aliquam amet quos expedita ullam autem nihil error, sint eveniet suscipit natus, debitis labore omnis excepturi.</p>
 </div>
 <?php 
    get_footer()
