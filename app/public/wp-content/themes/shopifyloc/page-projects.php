7f080e010c3ce001a72b5953262aa7c7b9a29109
LaupWing <loc_strict@hotmail.com>
2023-10-30 09:35:49 +0100

    dynamic query

--- app/public/wp-content/themes/shopifyloc/page-projects.php
+++ app/public/wp-content/themes/shopifyloc/page-projects.php
@@ -7,17 +7,25 @@
       <p class="text-slate-500 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
    </div>
 </div>
-<div class="mx-auto mt-10 flex flex-col w-full max-w-5xl">
+<div class="mx-auto my-16 flex flex-col w-full max-w-5xl">
    <ul class="grid grid-cols-2 gap-2">
       <?php 
-         $homepagePosts = new WP_Query(array(
-            "category__in" => array(get_cat_ID("projects"))
+         $current_page = get_post();
+         $parent_page_slug = $current_page->post_name;
+         
+         $parent_page = get_page_by_path($parent_page_slug);
+         $query = new WP_Query(array(
+            "post_type" => "page",
+            "post_status" => "publish",
+            "posts_per_page" => -1,
+            "post_parent" => $parent_page->ID
          ));
-         if(have_posts()){
-            while($homepagePosts->have_posts()){
-               $homepagePosts->the_post();
+         
+         if($query->have_posts()){
+            while($query->have_posts()){
+               $query->the_post();
                
-               $index = $homepagePosts->current_post;
+               $index = $query->current_post;
                get_template_part("templates/cards/blog-card");
             }
             wp_reset_postdata();
