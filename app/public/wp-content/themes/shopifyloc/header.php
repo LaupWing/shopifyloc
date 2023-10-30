2a7ccf619abe3ae8c051db833b7d4374e84b7087
LaupWing <loc_strict@hotmail.com>
2023-10-30 10:05:49 +0100

    udpate

--- app/public/wp-content/themes/shopifyloc/header.php
+++ app/public/wp-content/themes/shopifyloc/header.php
@@ -18,7 +18,7 @@
             <strong>Shopify</strong>Loc
          </i>
       </h1>
-      <nav class="border-l border-black flex">
+      <nav id="desktop" class="border-l border-black flex">
          <?php
             wp_nav_menu([
                "theme_location" => "headerMenuLocation"
@@ -63,3 +63,30 @@
          ?>
       </div>
    </header>
+
+   <!-- <div class="fixed inset-0 bg-white z-50 border-r-2 border-black">
+      <ul class="flex flex-col px-6 pt-16 pb-10">
+         <li>
+            <h1 class="px-4 flex text-slate-800 text-lg items-center">
+               <?php 
+                  $custom_class = "w-5 h-5 text-emerald-500 mr-1";
+                  get_template_part("templates/icons/logo" , null, array(
+                     "custom_class" => $custom_class
+                  ))
+               ?>
+               <i>
+                  <strong>Shopify</strong>Loc
+               </i>
+            </h1>
+         </li>
+         <li>
+            <nav>
+               <?php
+                  wp_nav_menu([
+                     "theme_location" => "headerMenuLocation"
+                  ]);
+               ?>
+            </nav>
+         </li>
+      </ul>
+   </div> -->
