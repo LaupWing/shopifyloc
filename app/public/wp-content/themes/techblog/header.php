e82455bcc8e0a5c731f5e82150f38ec2e70452f3
LaupWing <loc_strict@hotmail.com>
2023-10-31 19:01:46 +0100

    added border sky color

--- app/public/wp-content/themes/techblog/header.php
+++ app/public/wp-content/themes/techblog/header.php
@@ -5,8 +5,8 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <?php wp_head() ?>
 </head>
-<body class="bg-slate-50 min-h-screen flex flex-col" <?php body_class() ?>>
-   <header class="w-full hidden sm:flex h-14 bg-white border-b border-black">
+<body class="bg-slate-900 min-h-screen flex flex-col" <?php body_class() ?>>
+   <header class="w-full hidden sm:flex h-14 bg-slate-900 border-b border-sky-200">
       <h1 class="px-4 flex text-slate-800 text-lg items-center">
          <?php 
             $custom_class = "w-5 h-5 text-emerald-500 mr-1";
